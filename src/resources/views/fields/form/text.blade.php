d7cc92817d3e0d24a24b011aa47cd8b669b41b6e
Alex Chalupka <lupka31@gmail.com>
2016-11-13 21:47:31 -0600

    fix weird space adding

--- src/resources/views/fields/form/text.blade.php
+++ src/resources/views/fields/form/text.blade.php
@@ -1,4 +1,4 @@
 <fieldset class="form-group">
     <label for="name">{{ $field->getConfig('label') }}</label>
-    <input type="text" name="{{ $fieldName }}" class="form-control" value="@if($model) {{ $model->{$fieldName} }} @endif">
+    <input type="text" name="{{ $fieldName }}" class="form-control" value="@if($model){{$model->{$fieldName}}}@endif">
 </fieldset>
